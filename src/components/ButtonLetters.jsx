a5c5d22e4f696f7759faa78ccbc36e2065a78660
Deivid Rodriguez <34578919+drodriguez28@users.noreply.github.com>
2022-05-09 10:49:31 -0400

    formatting

--- src/components/ButtonLetters.jsx
+++ src/components/ButtonLetters.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const ButtonLetters = () => {
+  const alphabetLetters = "abcdefghijklmnopqrstuvwxyz".split("");
 
-const alphabetLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
-
-  return ( 
-  <div className="alphabet-letters-container">
-    {alphabetLetters.map((btn) => (
+  return (
+    <div className="alphabet-letters-container">
+      {alphabetLetters.map((btn) => (
         <button
           className="alphabet-letters"
         >
           {btn}
         </button>
       ))}
-  </div>
-    );
+    </div>
+  );
 };
 
 export default ButtonLetters;
