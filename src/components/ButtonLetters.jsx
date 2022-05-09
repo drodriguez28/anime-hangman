6ab21bed907304bcf891598dba70c44d8b589c7b
Deivid Rodriguez <34578919+drodriguez28@users.noreply.github.com>
2022-05-09 17:55:29 -0400

    using console log

--- src/components/ButtonLetters.jsx
+++ src/components/ButtonLetters.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const ButtonLetters = () => {
   const alphabetLetters = "abcdefghijklmnopqrstuvwxyz".split("");
-
+  
   return (
     <div className="alphabet-letters-container">
-      {alphabetLetters.map((btn) => (
+      {alphabetLetters.map((btn, index) => (
         <button
+          key={index}
           className="alphabet-letters"
+          onClick={() => console.log(index)}
         >
           {btn}
         </button>
