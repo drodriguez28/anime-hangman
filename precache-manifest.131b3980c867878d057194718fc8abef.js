e68e6a855222e558406b71b2438194d49471e8d0
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2022-05-21 20:59:27 -0400

    Updates

---
+++ precache-manifest.131b3980c867878d057194718fc8abef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "727ee838eb0009c506ef",
+    "url": "/anime-hangman/static/css/main.81201d0a.chunk.css"
+  },
+  {
+    "revision": "727ee838eb0009c506ef",
+    "url": "/anime-hangman/static/js/main.727ee838.chunk.js"
+  },
+  {
+    "revision": "521dee7d6a1a5478516b",
+    "url": "/anime-hangman/static/js/1.521dee7d.chunk.js"
+  },
+  {
+    "revision": "f5603992fd358a58ef15",
+    "url": "/anime-hangman/static/js/2.f5603992.chunk.js"
+  },
+  {
+    "revision": "65d25a07e26286e7126b",
+    "url": "/anime-hangman/static/js/runtime~main.65d25a07.js"
+  },
+  {
+    "revision": "d30b2a7ca9cf8cc32d2b4d7d3892268a",
+    "url": "/anime-hangman/index.html"
+  }
+];
